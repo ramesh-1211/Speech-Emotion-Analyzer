8138fd82e242e5fd7d9d094b228d4e0e47896ea9
Venkata Ramesh K <kvram2511@gmail.com>
2025-12-16 20:34:26 +0530

    Update extract_features.py

--- extract_features.py
+++ extract_features.py
@@ -1,2 +1,12 @@
 import librosa
 import numpy as np
+
+def extract_mfcc(file_path, n_mfcc=40):
+    try:
+        audio, sr = librosa.load(file_path, duration=3, offset=0.5)
+        mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=n_mfcc)
+        mfcc_mean = np.mean(mfcc.T, axis=0)
+        return mfcc_mean
+    except Exception as e:
+        print(f"Error processing {file_path}: {e}")
+        return None
