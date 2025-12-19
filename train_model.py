d0ff35f21a2250acd804cccdbc616f4f29750240
Venkata Ramesh K <kvram2511@gmail.com>
2025-12-19 17:49:11 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,3 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+import joblib
