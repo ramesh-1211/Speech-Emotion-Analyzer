e7558ae53c26fba1f99c716fb8a5ddf2189c1052
Venkata Ramesh K <kvram2511@gmail.com>
2025-12-20 16:46:39 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -4,3 +4,12 @@ from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 import joblib
+X = np.load("X_features.npy")
+y = np.load("y_labels.npy")
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = Pipeline([("scaler", StandardScaler()),("svm", SVC(kernel="rbf", probability=True))])
+model.fit(X_train, y_train)
+
+joblib.dump(model, "emotion_model.pkl")
+print("Model trained and saved successfully!")
