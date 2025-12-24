3610f2ab0eaf6f5650cba342a1928faa9492bf69
Venkata Ramesh Kesana <kvram2511@gmail.com>
2025-12-24 22:45:19 +0530

    Update evaluate_model.py

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,3 +1,8 @@
 import numpy as np
 import joblib
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+X = np.load("X_features.npy")
+y = np.load("y_labels.npy")
+model = joblib.load("emotion_model.pkl")
+
+y_pred = model.predict(X)
