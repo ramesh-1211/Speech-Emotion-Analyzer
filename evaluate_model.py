cecf714bf5b7ba105fae6ec3e486be31e7da97b1
Venkata Ramesh Kesana <kvram2511@gmail.com>
2026-01-03 17:30:21 +0530

    Update evaluate_model.py

--- evaluate_model.py
+++ evaluate_model.py
@@ -4,5 +4,9 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 X = np.load("X_features.npy")
 y = np.load("y_labels.npy")
 model = joblib.load("emotion_model.pkl")
-
 y_pred = model.predict(X)
+print("Accuracy:", accuracy_score(y, y_pred))
+print("\nClassification Report:\n")
+print(classification_report(y, y_pred, target_names=["happy", "sad", "angry", "neutral"]))
+print("Confusion Matrix:\n")
+print(confusion_matrix(y, y_pred))
