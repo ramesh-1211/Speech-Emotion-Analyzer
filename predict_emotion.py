e0a04978800c87c3ccae5f7433bb1b366b2e376b
Venkata Ramesh Kesana <kvram2511@gmail.com>
2026-01-03 17:34:09 +0530

    Update predict_emotion.py

--- predict_emotion.py
+++ predict_emotion.py
@@ -3,3 +3,6 @@ from extract_features import extract_mfcc
 import sounddevice as sd
 from scipy.io.wavfile import write
 import numpy as np
+
+EMOTIONS = ["happy", "sad", "angry", "neutral"]
+model = joblib.load("emotion_model.pkl")
